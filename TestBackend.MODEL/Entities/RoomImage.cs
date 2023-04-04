f2508dafe4fbad8cc186bd2ca7c6c9ad468c3b73
OmarL51 <omar.lozano@distrimedical.com.co>
2023-04-04 15:26:03 -0500

    Add enity RoomImage

---
+++ TestBackend.MODEL/Entities/RoomImage.cs
@@ -0,0 +1,11 @@
+﻿namespace TestBackend.MODEL.Entities
+{
+    public class RoomImage
+    {
+        public int imageId { get; set; }
+        public int roomId { get; set; }
+        public string imageTittle { get; set; }
+        public string imageDescription { get; set; }
+        public string imageUrl { get; set; }
+    }
+}
