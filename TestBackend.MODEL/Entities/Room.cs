3cfd04eb424e41081d95d30bd1f8591de19b7ab8
OmarL51 <omar.lozano@distrimedical.com.co>
2023-04-04 13:44:40 -0500

    Add entity Room

---
+++ TestBackend.MODEL/Entities/Room.cs
@@ -0,0 +1,12 @@
+﻿namespace TestBackend.MODEL.Entities
+{
+    public class Room
+    {
+        public int idRoom { get; set; }
+        public int idHotel { get; set; }
+        public string nameRoom { get; set; }
+        public string descriptionRoom { get; set;}
+        public int idType { get; set; }
+        public int idLocation { get; set; }
+    }
+}
