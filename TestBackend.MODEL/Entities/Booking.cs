76c09193992c269cbc825dc9f7a8966fedfa9645
OmarL51 <omar.lozano@distrimedical.com.co>
2023-04-04 14:06:58 -0500

    Add entity Booking

---
+++ TestBackend.MODEL/Entities/Booking.cs
@@ -0,0 +1,17 @@
+﻿namespace TestBackend.MODEL.Entities
+{
+    public class Booking
+    {
+        public int idBooking { get; set; }
+        public int idHotel { get; set; }
+        public int typeIdentification { get; set; }
+        public int identificationUser { get; set; }
+        public string firstName { get; set;}
+        public string secondName { get; set;}   
+        public string firstLastName { get; set;}
+        public string secondLastName { get; set;}
+        public string emailUser { get; set; }
+
+
+    }
+}
