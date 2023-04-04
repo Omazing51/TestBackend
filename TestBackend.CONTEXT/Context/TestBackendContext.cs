f25ab75065e0494407e8b02a84535bd45a81ec3f
OmarL51 <omar.lozano@distrimedical.com.co>
2023-04-04 16:21:06 -0500

    Add and configuration TestBackendContext class

---
+++ TestBackend.CONTEXT/Context/TestBackendContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestBackend.MODEL.Entities;
+
+namespace TestBackend.CONTEXT.Context
+{
+    public class TestBackendContext : DbContext
+    {
+        public TestBackendContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HotelService>().HasKey(x => new { x.hotelId, x.serviceId });
+            modelBuilder.Entity<RoomBooking>().HasKey(x => new { x.roomId, x.bookingId });
+        }
+
+        public DbSet<Booking> Bookings { get; set; } 
+        public DbSet<BookingDetail> BookingDetails { get; set; }
+        public DbSet<BookingStatus> BookingStatuses { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<HotelClass> HotelClasses { get; set; }
+        public DbSet<HotelImage> HotelImages { get; set; }
+        public DbSet<HotelService> HotelServices { get; set; }
+        public DbSet<HotelStatus> HotelStatuses { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomBooking> RoomBookings { get; set; }
+        public DbSet<RoomImage> RoomImages { get; set; }
+        public DbSet<RoomStatus> RoomStatuses { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Service> Services { get; set; }
+
+
+    }
+}
