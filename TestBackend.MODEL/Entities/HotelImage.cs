2f25ac358782c13cabd9cd8ee2f89532d2c671c0
OmarL51 <omar.lozano@distrimedical.com.co>
2023-04-04 15:24:24 -0500

    Add entity HotelImage

---
+++ TestBackend.MODEL/Entities/HotelImage.cs
@@ -0,0 +1,13 @@
+﻿namespace TestBackend.MODEL.Entities
+{
+    public class HotelImage
+    {
+        public int imageId { get; set; }
+        public int hotelId { get; set; }
+        public string imageTittle { get; set; }
+        public string imageDescription { get; set; }
+        public string imageUrl { get; set; }
+
+
+    }
+}
