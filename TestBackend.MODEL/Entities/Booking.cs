5cd8787ac2c655e009d64d880efdf35f05af9e5d
OmarL51 <omar.lozano@distrimedical.com.co>
2023-04-04 14:09:50 -0500

    Add entity BookingDetail

--- TestBackend.MODEL/Entities/Booking.cs
+++ TestBackend.MODEL/Entities/Booking.cs
@@ -4,14 +4,7 @@
     {
         public int idBooking { get; set; }
         public int idHotel { get; set; }
-        public int typeIdentification { get; set; }
-        public int identificationUser { get; set; }
-        public string firstName { get; set;}
-        public string secondName { get; set;}   
-        public string firstLastName { get; set;}
-        public string secondLastName { get; set;}
-        public string emailUser { get; set; }
-
+        public int idStatus { get; set; }
 
     }
 }
---
+++ TestBackend.MODEL/Entities/BookingDetail.cs
@@ -0,0 +1,21 @@
+﻿namespace TestBackend.MODEL.Entities
+{
+    public class BookingDetail
+    {
+        public int idBookingDetail { get; set; }
+        public int idBooking { get; set; }
+        public int typeIdentification { get; set; }
+        public int identificationUser { get; set; }
+        public string firstName { get; set; }
+        public string? secondName { get; set; }
+        public string firstLastName { get; set; }
+        public string? secondLastName { get; set; }
+        public DateTime birthdayUser { get; set; }
+        public string emailUser { get; set; }
+        public string phoneNumberUser { get; set; }
+        public string contactName { get; set; }
+        public string phoneNumberContact { get; set; }
+        public DateTime startBooking { get; set; }
+        public DateTime endBooking { get; set; }
+    }
+}
