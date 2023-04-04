893e593f916ee99b7080b148db4c3cf3aeffe049
OmarL51 <omar.lozano@distrimedical.com.co>
2023-04-04 14:36:54 -0500

    Add entity HotelClass

---
+++ TestBackend.MODEL/Entities/HotelClass.cs
@@ -0,0 +1,9 @@
+﻿namespace TestBackend.MODEL.Entities
+{
+    public class HotelClass
+    {
+        public int hotelClassId { get; set; }
+        public string hotelClassName { get; set; }
+        public string hotelClassDescription { get; set; }   
+    }
+}
