68b683fefe767377f5b55c3108c51f6b7acc736a
OmarL51 <omar.lozano@distrimedical.com.co>
2023-04-05 11:01:14 -0500

    Migrate HotelImages entity

---
+++ TestBackend.CONTEXT/Migrations/20230405160045_hotelimages.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestBackend.CONTEXT.Migrations
+{
+    /// <inheritdoc />
+    public partial class hotelimages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HotelImages",
+                columns: table => new
+                {
+                    imageId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    imageTittle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    imageDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    imageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelImages", x => x.imageId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HotelImages");
+        }
+    }
+}
