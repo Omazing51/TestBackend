4dcb79d9b851ec049dfbb4e9bd4db6cd5f18af38
OmarL51 <omar.lozano@distrimedical.com.co>
2023-04-04 21:11:28 -0500

    Changes on Roomtype entity

--- TestBackend.MODEL/Entities/RoomType.cs
+++ TestBackend.MODEL/Entities/RoomType.cs
@@ -4,5 +4,10 @@
     {
         public int typeId { get; set; }
         public string DescriptionType { get; set; }
+        public int totalPerson { get; set; }
+        public int? totalToilet { get; set; }
+        public int? totalJacuzzi { get; set; }
+        public int? totalTv { get; set; }
+        public decimal totalValue { get; set; }
     }
 }
