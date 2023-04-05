32a79a5dc331cdff08c5cbfc8b5c906b49d242e3
OmarL51 <omar.lozano@distrimedical.com.co>
2023-04-04 21:15:16 -0500

    Add entity User

---
+++ TestBackend.MODEL/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TestBackend.MODEL.Entities
+{
+    public class User : IdentityUser
+    {
+        public string userPassword { get; set; }
+    }
+}
