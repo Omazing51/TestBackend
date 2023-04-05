eec0a04a78e11773df5ff0d577675ba58f0008be
OmarL51 <omar.lozano@distrimedical.com.co>
2023-04-05 10:42:21 -0500

    Migrate Cities entity

--- TestBackend.CONTEXT/Context/TestBackendContext.cs
+++ TestBackend.CONTEXT/Context/TestBackendContext.cs
@@ -21,7 +21,7 @@ namespace TestBackend.CONTEXT.Context
         //public DbSet<Booking> Bookings { get; set; }
         //public DbSet<BookingDetail> BookingDetails { get; set; }
         public DbSet<BookingStatus> BookingStatuses { get; set; }
-        //public DbSet<City> Cities { get; set; }
+        public DbSet<City> Cities { get; set; }
         public DbSet<Country> Countries { get; set; }
         //public DbSet<Hotel> Hotels { get; set; }
         //public DbSet<HotelClass> HotelClasses { get; set; }
---
+++ TestBackend.CONTEXT/Migrations/20230405154116_cities.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestBackend.CONTEXT.Migrations
+{
+    /// <inheritdoc />
+    public partial class cities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    cityId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    cityName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.cityId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
