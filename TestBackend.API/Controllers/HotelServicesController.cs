f3d5bcbbd89c8b9206536f86ab5d673657e1bafe
omaryeso <67795148+omaryeso@users.noreply.github.com>
2023-04-06 11:15:34 -0500

    Add HotelServices API

---
+++ TestBackend.API/Controllers/HotelServicesController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestBackend.APP.Application.HotelServices;
+using TestBackend.APP.Application.Services;
+using TestBackend.MODEL.Entities;
+
+namespace TestBackend.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelServicesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public HotelServicesController(IMediator mediator)
+        {
+            _mediator = mediator;   
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<HotelService>>> Get()
+        {
+            return await _mediator.Send(new GetHotelService.HotelServicesList());
+        }
+
+        
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(PostHotelService.ExecuteHS data)
+        {
+            return await _mediator.Send(data);
+        }
+
+    }
+}
