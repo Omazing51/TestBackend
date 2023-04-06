516e97586b3466cb1a25c54d87521beaf466b738
omaryeso <67795148+omaryeso@users.noreply.github.com>
2023-04-06 09:49:14 -0500

    Add Locations API

---
+++ TestBackend.API/Controllers/LocationsController.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestBackend.APP.Application.Locations;
+using TestBackend.MODEL.Entities;
+
+namespace TestBackend.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public LocationsController(IMediator mediator)
+        {
+            _mediator = mediator;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Location>>> Get()
+        {
+            return await _mediator.Send(new GetLocation.LocationsList());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Location>> Detail(int id)
+        {
+            return await _mediator.Send(new GetLocationById.UniqueLocation { locationId = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(PostLocation.ExecuteLocation data)
+        {
+            return await _mediator.Send(data);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(int id, PutLocation.EditLocation data)
+        {
+            data.locationId = id;
+            return await _mediator.Send(data);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(int id)
+        {
+            return await _mediator.Send(new DelLocation.DeleteLocation{ locationId = id });
+        }
+
+    }
+}
