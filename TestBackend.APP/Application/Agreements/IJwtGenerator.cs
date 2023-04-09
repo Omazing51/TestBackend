6e1f7ab9f474c9a0e86fa779c1a1a70fea756c17
omaryeso <67795148+omaryeso@users.noreply.github.com>
2023-04-09 18:23:53 -0500

    Add Agreements folder

---
+++ TestBackend.APP/Application/Agreements/IJwtGenerator.cs
@@ -0,0 +1,9 @@
+﻿using TestBackend.MODEL.Entities;
+
+namespace TestBackend.APP.Application.Agreements
+{
+    public interface IJwtGenerator
+    {
+        string CreateToken(User user);
+    }
+}
